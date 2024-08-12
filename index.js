63c1a1a323339619a749d9215ac7c30e57968d10
Ifeanyi <72707830+Ifeanyi55@users.noreply.github.com>
2024-08-12 13:00:16 +0100

    Application files

---
+++ index.js
@@ -0,0 +1,24 @@
+const { createServer } = require("node:http");
+const fs = require("node:fs");
+
+const port = 3000;
+const hostname = "0.0.0.0";
+
+const server = createServer((req,res) => {
+    fs.readFile("eBird.json", function(err, data){
+        if(err){
+            res.statusCode = 400;
+            res.end("Error in connection!");
+            console.log("Failed to establish connection");
+        } else {
+            res.statusCode = 200,
+            res.setHeader("Content-Type", "text/json");
+            res.write(data);
+            return res.end();
+        };
+    });
+});
+
+server.listen(port,hostname, () => {
+    console.log(`Server listening at http://${hostname}:${port}/`);
+});
